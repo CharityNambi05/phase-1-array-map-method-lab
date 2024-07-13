2189cb238f57942c515e1226d0bb33d11d1fdf51
CharityNambi05 <charity.nambi@icloud.com>
2024-07-13 20:13:57 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,9 +1,10 @@
+
 const tutorials = [
   'what does the this keyword mean?',
   'What is the Constructor OO pattern?',
   'implementing Blockchain Web API',
   'The Test Driven Development Workflow',
-  'What is NaN and how Can we Check for it',
+  'What is NaN and how can we Check for it',
   'What is the difference between stopPropagation and preventDefault?',
   'Immutable State and Pure Functions',
   'what is the difference between == and ===?',
@@ -11,6 +12,15 @@ const tutorials = [
   'what is JSONP?'
 ];
 
-const titleCased = () => {
-  return tutorials
+
+function titleCased() {
+  return tutorials.map(tutorial => {
+    return tutorial.split(' ').map(word => {
+      return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+    }).join(' ');
+  });
 }
+
+
+console.log(titleCased());
+
